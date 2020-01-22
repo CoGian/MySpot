2779114e3604615ade62edc7a11ee86591e91184
georgekal2798 <38210685+georgekal2798@users.noreply.github.com>
2020-01-23 01:26:14 +0200

    changed alarm activity. replaced timepicker and switch with a time editText

--- app/src/main/java/com/example/myspot/AlarmActivity.java
+++ app/src/main/java/com/example/myspot/AlarmActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -33,14 +35,15 @@ import static java.lang.Math.floor;
 public class AlarmActivity extends AppCompatActivity {
 
     private AlarmManager alarmManager;
-    private TimePicker alarm_timePicker ;
+    private TimePickerDialog alarm_timePickerDialog ;
     private Context context;
     private PendingIntent pendingIntent;
     private Calendar calendar;
     private Intent rec_intent;
-    private Switch sb ;
+//    private Switch sb ;
     private EditText initialCostText ;
     private EditText costPerHourText;
+    private EditText alarmTime;
     private TextView finalCostView ;
     private  int hour ;
     private  int minute ;
@@ -72,14 +75,29 @@ public class AlarmActivity extends AppCompatActivity {
         // initialize alarm manager
         alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
 
-        // initialize timepicker
-        alarm_timePicker = findViewById(R.id.alarmTimePicker);
+        // initialize alarmTime edit text
+        alarmTime = findViewById(R.id.editTextAlarm);
+
+        alarmTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alarm_timePickerDialog = new TimePickerDialog(AlarmActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfHour) {
+                        alarmTime.setText(hourOfDay + ":" + minuteOfHour);
+                        hour = hourOfDay;
+                        minute = minuteOfHour;
+                    }
+                }, 0,0,true);
+                alarm_timePickerDialog.show();
+            }
+        });
 
         // create an intent to the Alarm Receiver
         rec_intent = new Intent(this.context,Alarm_Receiver.class);
 
         // initialize switch
-        sb = findViewById(R.id.switchAlarm);
+//        sb = findViewById(R.id.switchAlarm);
 
         Intent intent = getIntent();
 
@@ -102,63 +120,63 @@ public class AlarmActivity extends AppCompatActivity {
             // calculate final cost
             double final_cost = calc_final_cost();
             finalCostView.setText("Final cost: "+df.format(final_cost)+" €");
-            if (alarmOn)
-                sb.setText("Alarm On");
-            else
-                sb.setText("Alarm Off");
+//            if (alarmOn)
+//                sb.setText("Alarm On");
+//            else
+//                sb.setText("Alarm Off");
 
 
         }
 
         // create on checked change listener
-        sb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked && !alarmOn){
-                    sb.setText("Alarm On");
-                    alarmOn = true;
-
-                    // calculate final cost
-                    double final_cost = calc_final_cost();
-
-                    // get the string values of hour and minute
-                    hour = alarm_timePicker.getHour();
-                    minute= alarm_timePicker.getMinute();
-
-
-                    rec_intent.putExtra("final_cost",final_cost);
-
-                    // convert the int values to string
-                    String hour_str = String.valueOf(hour);
-                    String min_str = String.valueOf(minute);
-                    if(minute<10)
-                        min_str = "0" + String.valueOf(minute);
-                    Log.e("ALARM", "Alarm set on: " + hour_str + ":" + min_str + "   on " + String.valueOf(calendar.get(Calendar.DAY_OF_MONTH))+ " with cost: " + final_cost);
-
-                    //create a pending intent that delays the intent
-                    // until the specified calendar time
-                    pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this,0,rec_intent
-                    ,PendingIntent.FLAG_UPDATE_CURRENT);
-
-                    //set the alarm manager
-                    alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis()
-                    ,pendingIntent);
-
-                    Toast.makeText(context,"Alarm is on.", Toast.LENGTH_SHORT).show();
-                }else if(!isChecked && alarmOn){
-
-                    pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this,0,rec_intent
-                            ,PendingIntent.FLAG_UPDATE_CURRENT);
-
-                    sb.setText("Alarm Off");
-                    alarmOn = false;
-                    alarmManager.cancel(pendingIntent);
-                    Toast.makeText(context,"Alarm is off.", Toast.LENGTH_SHORT).show();
-                    Log.e("ALARM", " ia m here");
-                }
-
-            }
-        });
+//        sb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked && !alarmOn){
+//                    sb.setText("Alarm On");
+//                    alarmOn = true;
+//
+//                    // calculate final cost
+//                    double final_cost = calc_final_cost();
+//
+//                    // get the string values of hour and minute
+//                    hour = alarm_timePicker.getHour();
+//                    minute= alarm_timePicker.getMinute();
+//
+//
+//                    rec_intent.putExtra("final_cost",final_cost);
+//
+//                    // convert the int values to string
+//                    String hour_str = String.valueOf(hour);
+//                    String min_str = String.valueOf(minute);
+//                    if(minute<10)
+//                        min_str = "0" + String.valueOf(minute);
+//                    Log.e("ALARM", "Alarm set on: " + hour_str + ":" + min_str + "   on " + String.valueOf(calendar.get(Calendar.DAY_OF_MONTH))+ " with cost: " + final_cost);
+//
+//                    //create a pending intent that delays the intent
+//                    // until the specified calendar time
+//                    pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this,0,rec_intent
+//                    ,PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//                    //set the alarm manager
+//                    alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis()
+//                    ,pendingIntent);
+//
+//                    Toast.makeText(context,"Alarm is on.", Toast.LENGTH_SHORT).show();
+//                }else if(!isChecked && alarmOn){
+//
+//                    pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this,0,rec_intent
+//                            ,PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//                    sb.setText("Alarm Off");
+//                    alarmOn = false;
+//                    alarmManager.cancel(pendingIntent);
+//                    Toast.makeText(context,"Alarm is off.", Toast.LENGTH_SHORT).show();
+//                    Log.e("ALARM", " ia m here");
+//                }
+//
+//            }
+//        });
 
         // create on text changed listener
         initialCostText.addTextChangedListener(new TextWatcher() {
@@ -199,14 +217,33 @@ public class AlarmActivity extends AppCompatActivity {
             }
         });
 
-        alarm_timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+        alarmTime.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // calculate final cost
                 double final_cost = calc_final_cost();
                 finalCostView.setText("Final cost: "+df.format(final_cost)+" €");
             }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
+
+//        alarm_timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+//            @Override
+//            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+//                // calculate final cost
+//                double final_cost = calc_final_cost();
+//                finalCostView.setText("Final cost: "+df.format(final_cost)+" €");
+//            }
+//        });
     }
 
     @Override
@@ -243,9 +280,9 @@ public class AlarmActivity extends AppCompatActivity {
         calendar = Calendar.getInstance();
         Calendar curr_cal = Calendar.getInstance();
         // setting calendar instance with the hour and minutes that picked
-        calendar.set(Calendar.HOUR_OF_DAY,alarm_timePicker.getHour());
-        calendar.set(Calendar.MINUTE,alarm_timePicker.getMinute());
-        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
         Log.e("ALARM","" + calendar.getTimeInMillis()+ "     " + curr_cal.getTimeInMillis());
         // check if user has put time from tomorrow and increase calendars' day by one
         if (curr_cal.getTimeInMillis()>calendar.getTimeInMillis())
@@ -264,6 +301,7 @@ public class AlarmActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_save:
+                //save to database
                 DB.addParking(new Parking(
                     location,
                     initial_cost,
@@ -272,7 +310,51 @@ public class AlarmActivity extends AppCompatActivity {
                     duration,
                     alarmOn,
                     true
-            ));
+                ));
+
+                //add alarm
+                if(!alarmOn){
+                    alarmOn = true;
+
+                    // calculate final cost
+                    double final_cost = calc_final_cost();
+
+                    // get the string values of hour and minute
+//                    hour = alarm_timePicker.getHour();
+//                    minute= alarm_timePicker.getMinute();
+
+
+                    rec_intent.putExtra("final_cost",final_cost);
+
+                    // convert the int values to string
+                    String hour_str = String.valueOf(hour);
+                    String min_str = String.valueOf(minute);
+                    if(minute<10)
+                        min_str = "0" + String.valueOf(minute);
+                    Log.e("ALARM", "Alarm set on: " + hour_str + ":" + min_str + "   on " + String.valueOf(calendar.get(Calendar.DAY_OF_MONTH))+ " with cost: " + final_cost);
+
+                    //create a pending intent that delays the intent
+                    // until the specified calendar time
+                    pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this,0,rec_intent
+                            ,PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    //set the alarm manager
+                    alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis()
+                            ,pendingIntent);
+
+                    Toast.makeText(context,"Alarm is on.", Toast.LENGTH_SHORT).show();
+                }
+//                else if(alarmOn){
+//
+//                    pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this,0,rec_intent
+//                            ,PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//                    alarmOn = false;
+//                    alarmManager.cancel(pendingIntent);
+//                    Toast.makeText(context,"Alarm is off.", Toast.LENGTH_SHORT).show();
+//                    Log.e("ALARM", " ia m here");
+//                }
+
                 startActivity(new Intent(this, MapsActivity.class));
                 return true;
             default:
